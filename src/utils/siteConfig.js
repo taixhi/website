d4b3751e73ef91d59d85536bad551fdea293b8aa
Taichi Kato <me@taichikato.com>
2018-11-03 17:51:47 +0800

    Configured

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,21 +1,20 @@
 module.exports = {
-  siteTitle: 'GCN',
-  siteTitleAlt: 'GCN Gatsby Starter', // This allows an alternative site title for SEO schema.
-  publisher: 'Publisher named GCN', // Organization name used for SEO schema
-  siteDescription:
-    'A starter template to build amazing static websites with Gatsby, Contentful and Netlify',
-  siteUrl: 'https://gcn.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
-  postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
+  siteTitle: 'Taichi Kato',
+  siteTitleAlt: 'Taichi Kato (@taixhi)', // This allows an alternative site title for SEO schema.
+  publisher: 'Taichi Kato', // Organization name used for SEO schema
+  siteDescription: 'Hi I\'m Taichi, I enjoy writing code, exploring/photographing the world, and meeting awesome people. I\'m currently studying in Singapore.',
+  siteUrl: 'https://taichikato.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  postsPerHomePage: 8, // Number of posts shown on the 1st page of of the index.js template (home page)
   postsPerPage: 6, // Number of posts shown on paginated pages
-  author: 'GCN User', // Author for RSS author segment and SEO schema
-  authorUrl: 'https://gcn.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
-  userTwitter: '@twitter', // Change for Twitter Cards
-  shortTitle: 'GCN App', // Used for App manifest e.g. Mobile Home Screen
+  author: 'Taichi Kato', // Author for RSS author segment and SEO schema
+  authorUrl: 'https://taichikato.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
+  userTwitter: '@kato_taichi', // Change for Twitter Cards
+  shortTitle: 'Taichi Kato', // Used for App manifest e.g. Mobile Home Screen
   shareImage: '/logos/share.jpg', // Open Graph Default Share Image. 1200x1200 is recommended
   shareImageWidth: 900, // Change to the width of your default share image
   shareImageHeight: 600, // Change to the height of your default share image
   siteLogo: '/logos/logo-512.png', // Logo used for SEO, RSS, and App manifest
   backgroundColor: '#e9e9e9', // Used for Offline Manifest
   themeColor: '#121212', // Used for Offline Manifest
-  copyright: 'Copyright © 2018 GCN User', // Copyright string for the RSS feed
+  copyright: 'Copyright © 2019 Taichi Kato', // Copyright string for the RSS feed
 }
